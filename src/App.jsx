0c18a2caa6191a1d52e15903594bbc4c2fe1e2ea
IKANYI <paulkaranja214@gmail.com>
2024-06-06 23:45:28 +0300

    fixed a network error in the code

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     .then(res => res.json())
     .then(
       (quote)=> {
-        setQuote(quote.advice);
+        setQuote(quote.slip.advice);
       }
     )
   },[])
@@ -19,7 +19,7 @@ function App() {
     .then(res => res.json())
     .then(
       (quote)=> {
-        setQuote(quote.advice);
+        setQuote(quote.slip.advice);
       }
     )
   }
